38d4f7e581866bfee331dc07531c5ec5c16fabf4
pratikshas29 <pratikshashitole29@gmail.com>
2025-02-25 14:50:07 +0530

    v-1.1.9

--- src/components/Login.js
+++ src/components/Login.js
@@ -310,9 +310,9 @@ localStorage.setItem('refresh',refresh)
             <div className="text-center mb-8">
               <div className="text-center mb-3 flex items-center justify-center">
                 <img src={MenuMitra} alt="MenuMitra Logo" className="w-10 h-10 mr-2" /> Adjust logo size
-                <h1 className="text-2xl font-bold text-gray-800 ">MenuMitra pos</h1>
+                <h1 className="text-2xl font-bold text-gray-800 ">MenuMitra pos pratiksha</h1>
               </div>
-              {/* <p className="text-gray-600">Sign in to continue to your account</p> */}
+              <p className="text-gray-600">Sign in to continue to your account</p>
             </div>
 
             <form onSubmit={showOtpField ? handleVerifyOtp : handleSendOtp} className="space-y-6">
